fa271689ad226b30bccb394e3a25d337a2582608
andresyjuan <Mallez1982@gmail.com>
2024-01-27 17:28:10 -0500

    [FTR] Initial proyect

---
+++ src/test/java/com/company/auditapi/AuditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.auditapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
